060562de9b5bae966796dfe4fc74734f7524385c
Ralph Schaer <ralphschaer@gmail.com>
2023-07-25 18:58:47 +0200

    Upgrade

--- server/src/main/java/ch/rasc/otodo/config/security/SecurityConfig.java
+++ server/src/main/java/ch/rasc/otodo/config/security/SecurityConfig.java
@@ -2,15 +2,19 @@ package ch.rasc.otodo.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.context.SecurityContextHolderFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 class SecurityConfig {
@@ -32,13 +36,20 @@ class SecurityConfig {
     };
   }
 
+  @Scope("prototype")
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+    return new MvcRequestMatcher.Builder(introspector);
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc)
+      throws Exception {
     var pathPrefix = "/be";
 
     // @formatter:off
     http
-    .headers(customizer -> customizer.defaultsDisabled().cacheControl())
+    .headers(customizer -> customizer.defaultsDisabled().cacheControl(Customizer.withDefaults()))
     .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
     .csrf(CsrfConfigurer::disable)
   	.logout(customizer -> {
@@ -47,16 +58,16 @@ class SecurityConfig {
   	})
     .authorizeHttpRequests(customizer -> {
       customizer.requestMatchers(
-          pathPrefix + "/login",
-          pathPrefix + "/signup",
-          pathPrefix + "/confirm-signup",
-          pathPrefix + "/reset-password-request",
-          pathPrefix + "/reset-password",
-          pathPrefix + "/confirm-email-change",
-          pathPrefix + "/client-error",
-          pathPrefix + "/csp-error")
+            mvc.pattern(pathPrefix + "/login"),
+            mvc.pattern(pathPrefix + "/signup"),
+            mvc.pattern(pathPrefix + "/confirm-signup"),
+            mvc.pattern(pathPrefix + "/reset-password-request"),
+            mvc.pattern(pathPrefix + "/reset-password"),
+            mvc.pattern(pathPrefix + "/confirm-email-change"),
+            mvc.pattern(pathPrefix + "/client-error"),
+            mvc.pattern(pathPrefix + "/csp-error"))
             .permitAll()
-          .requestMatchers(pathPrefix + "/admin/**").hasAuthority("ADMIN")
+          .requestMatchers(mvc.pattern(pathPrefix + "/admin/**")).hasAuthority("ADMIN")
       	  .anyRequest().authenticated();
     })
     .exceptionHandling(customizer -> customizer.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
